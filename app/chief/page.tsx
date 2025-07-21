26d7d1aebc8701960ef2c40a0c48a43895af43f8
thdat-vu <vuthanhdat181@gmail.com>
2025-07-21 12:21:41 +0700

    fix: fix className error

--- app/chief/page.tsx
+++ app/chief/page.tsx
@@ -6,6 +6,6 @@ export default function ChiefPage() {
 
   return <div>
     <h1>Chief Main Screen - Coming Soon</h1>
-    <button class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={() => router.push('/')}>Go to Home</button>
+    <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={() => router.push('/')}>Go to Home</button>
   </div>;
 } 
