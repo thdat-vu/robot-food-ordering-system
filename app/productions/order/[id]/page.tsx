3387032560c764041581ff39beac3af7f5b35700
nhattruowng <ltn04098@gmail.com>
2025-07-29 13:02:58 +0700

    fix

--- app/productions/order/[id]/page.tsx
+++ app/productions/order/[id]/page.tsx
@@ -3,6 +3,7 @@ import {OrderList} from "@/app/features/components/OrderList";
 
 export default function Page({params}: { params: Promise<{ id: string }> }) {
     const {id} = use(params)
+    
     return (
         <>
             <OrderList id={id}/>
