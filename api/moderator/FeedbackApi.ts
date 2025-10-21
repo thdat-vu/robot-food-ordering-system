daa542f3e30d5f690766be2ffb3cbed0ca554465
quoctrieu010903 <luongquoctrieu165@gmail.com>
2025-10-21 07:59:20 +0700

    update fix error code

--- api/moderator/FeedbackApi.ts
+++ api/moderator/FeedbackApi.ts
@@ -6,11 +6,22 @@ import {API_FEEDBACK} from "@/api-endpoint-env";
 export const GetFeedbackByIdtable = async (id: string): Promise<BaseEntityData<FeedbackgGetTableId[]>> => {
     try {
         const res = await api.get(`${API_FEEDBACK}/${id}`)
+         // 👇 Nếu res.data bị null hoặc không phải mảng
+    if (!res.data || !Array.isArray(res.data.data)) {
+        console.warn(`[GetFeedbackByIdtable] API trả về null hoặc sai định dạng.`);
+        return { data: [], message: 'Không có dữ liệu phản hồi.' } as any;
+      }
         return res.data;
-    } catch (err) {
-        throw err;
-    }
-}
+    } catch (error: any) {
+        const statusCode = error?.response?.status ?? 500;
+        const backendMsg =
+          error?.response?.data?.errorMessage ||
+          error?.response?.data?.message ||
+          'Không thể tải dữ liệu feedback.';
+          return { data: [], message: backendMsg } as any;
+      }
+    };
+
 
 
 export const GetAllFeedbackHome = async (): Promise<BaseEntityData<Record<string, TableData[]>>
